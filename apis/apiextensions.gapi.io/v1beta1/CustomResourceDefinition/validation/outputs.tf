8638d2c89c4fc6c55b8138700684cd1663b865c1
Lucas Campos Vieira <contato@lukerops.com>
2023-09-22 12:52:59 -0300

    feat: add duplicated crd version validation

--- apis/apiextensions.gapi.io/v1beta1/CustomResourceDefinition/validation/outputs.tf
+++ apis/apiextensions.gapi.io/v1beta1/CustomResourceDefinition/validation/outputs.tf
@@ -1,12 +1,12 @@
 locals {
   error_messages = {
     invalid_metadata_name = <<-EOT
-      Invalid "metadata.name" of CustomResourceDefinition "%s"!
+      Invalid "metadata.name" for CustomResourceDefinition "%s"!
       The name must be in the form "<plural>.<group>".
       (kind: %s; file: %s)
     EOT
     kind_camel_case       = <<-EOT
-      Invalid "names.kind" of CustomResourceDefinition "%s"!
+      Invalid "names.kind" for CustomResourceDefinition "%s"!
       The kind must be CamelCased.
       (kind: %s; file: %s)
     EOT
@@ -15,9 +15,27 @@ locals {
       The scope must be "global".
       (kind: %s; file: %s)
     EOT
+    duplicated_version    = <<-EOT
+      Duplicated versions "%#v" on CustomResourceDefinition "%s"!
+      (kind: %s; file: %s)
+    EOT
   }
 
   kind_first_letter = substr(var.manifest.spec.names.kind, 0, 1)
+
+  version_names = distinct([
+    for version in var.manifest.spec.versions : version.name
+  ])
+  version_names_count = {
+    for name in local.version_names : name => sum([
+      for version in var.manifest.spec.versions : 1
+      if version.name == name
+    ])
+  }
+  duplicated_versions = [
+    for name, count in local.version_names_count : name
+    if count > 1
+  ]
 }
 
 module "version" {
@@ -26,7 +44,7 @@ module "version" {
 
   file          = var.manifest._metadata.file
   metadata_name = var.manifest.metadata.name
-  crd_kind      = var.manifest.kind
+  crd_kind      = var.manifest.spec.names.kind
   crd_version   = var.manifest.spec.versions[count.index]
 }
 
@@ -63,7 +81,18 @@ output "manifest" {
     )
   }
 
-  depends_on = [ 
+  precondition {
+    condition = length(local.duplicated_versions) == 0
+    error_message = format(
+      local.error_messages.duplicated_version,
+      local.duplicated_versions,
+      var.manifest.metadata.name,
+      var.manifest.spec.names.kind,
+      var.manifest._metadata.file,
+    )
+  }
+
+  depends_on = [
     module.version,
   ]
 }
