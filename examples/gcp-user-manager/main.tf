e029a9c904c5b028fab473e58ad364a8770fbe4a
Lucas Campos Vieira <contato@lukerops.com>
2023-09-26 09:02:05 -0300

    refactor: simplify the example project

--- examples/gcp-user-manager/main.tf
+++ examples/gcp-user-manager/main.tf
@@ -2,14 +2,9 @@ locals {
   yamls          = fileset(path.module, "./manifests/*.{yml,yaml}")
   gcp_project_id = "your_project_id"
 
-  email_by_user = {
-    for _, resource in module.tf-gapi.resources["iam.cloud.google.com/v1beta1"].User :
-    (resource.metadata.name) => resource.spec.email
-  }
-
   roles_by_user = {
-    for _, resource in module.tf-gapi.resources["iam.cloud.google.com/v1beta1"].Permission :
-    (resource.spec.userRef.name) => resource.spec.roles
+    for _, resource in module.tf-gapi.resources["iam.cloud.google.com/v1beta1"].User :
+    (resource.spec.email) => resource.spec.roles
   }
 
   users_by_role = transpose(local.roles_by_user)
@@ -25,7 +20,7 @@ resource "google_project_iam_binding" "gapi_robe_bindings" {
 
   project = local.gcp_project_id
   role    = each.key
-  members = [for user in each.value : "user:${local.email_by_user[user]}"]
+  members = [for user in each.value : "user:${user}"]
 }
 
 output "tf-gapi" {
